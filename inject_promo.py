94c3572a5e2a751884bcb45e5ebe3c736354b098
chamdan55 <chamdanlab555@gmail.com>
2024-10-25 22:23:15 +0700

    update data, script inject, dan tambah file generate data

--- inject_promo.py
+++ inject_promo.py
@@ -53,29 +53,41 @@ def get_data(df):
 def insert_to_masteruser():
     # FREEPDB1.JABAR.
     sql_insert = """
-    INSERT INTO MASTERUSER
-    (USER_ID, GENDER, RELIGION, SEGMENTATION, TOTAL_ASSETS, ACCOUNT_AGE, TRANSACTION_TIME)
-    VALUES (:1, :2, :3, :4, :5, :6, :7)
+    INSERT INTO MASTER_USER
+    (ID, USER_ID, GENDER, RELIGION, TOTAL_ASSETS, ACCOUNT_AGE, TRANSACTION_TIME, SEGMENTATION, UPDATE_BY, UPDATED_TIME, CREATED_BY, CREATED_TIME)
+    VALUES (:1, :2, :3, :4, :5, :6, :7, :8, :9, TO_TIMESTAMP_TZ(:10, 'YYYY-MM-DD HH24:MI:SS TZH:TZM'), :11, TO_TIMESTAMP_TZ(:12, 'YYYY-MM-DD HH24:MI:SS TZH:TZM'))
     """
     return sql_insert
+
+
 
 def insert_to_mastermerchant():
     # SQL command to insert data
     # FREEPDB1.JABAR.
     sql_insert = """
-    INSERT INTO MASTERMERCHANT
-    (mcc_group, MERCHANT_CATEGORY)
-    VALUES (:1, :2)
+    INSERT INTO MASTER_MERCHANT
+    (ID, MCC_GROUP, MERCHANT_CATEGORY, UPDATE_BY, UPDATED_TIME, CREATED_BY, CREATED_TIME)
+    VALUES (:1, :2, :3, :4, TO_TIMESTAMP_TZ(:5, 'YYYY-MM-DD HH24:MI:SS TZH:TZM'), :6, TO_TIMESTAMP_TZ(:7, 'YYYY-MM-DD HH24:MI:SS TZH:TZM'))
     """
     return sql_insert
 
-def insert_to_masterpromo():
+def insert_to_masterpromo_1():
     # SQL command to insert data
     # FREEPDB1.JABAR.
     sql_insert = """
-    INSERT INTO MASTERPROMO
-    (PROMO_ID, MCC_GROUP, MERCHANT_CATEGORY, GENDER, RELIGION, PROMO_LOCATION, START_DATE, END_DATE, SEGMENTATION, TOTAL_ASSETS, ACCOUNT_AGE, TRANSACTION_TIME)
-    VALUES (:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, :12)
+    INSERT INTO MASTER_PROMO_1
+    (ID, PROMO_ID, MCC_GROUP, MERCHANT_CATEGORY, RELIGION, GENDER, SEGMENTATION, TOTAL_ASSETS, ACCOUNT_AGE, TRANSACTION_TIME, START_DATE, END_DATE, UPDATE_BY, UPDATED_TIME, CREATED_BY, CREATED_TIME)
+    VALUES (:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, :12, :13, TO_TIMESTAMP_TZ(:14, 'YYYY-MM-DD HH24:MI:SS TZH:TZM'), :15, TO_TIMESTAMP_TZ(:16, 'YYYY-MM-DD HH24:MI:SS TZH:TZM'))
+    """
+    return sql_insert
+
+def insert_to_masterpromo_2():
+    # SQL command to insert data
+    # FREEPDB1.JABAR.
+    sql_insert = """
+    INSERT INTO MASTER_PROMO_2
+    (ID, PROMO_ID, MCC_GROUP, MERCHANT_CATEGORY, RELIGION, GENDER, SEGMENTATION, PROMO_LOCATION, TOTAL_ASSETS, ACCOUNT_AGE, TRANSACTION_TIME, START_DATE, END_DATE, UPDATE_BY, UPDATED_TIME, CREATED_BY, CREATED_TIME)
+    VALUES (:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, :12, :13, :14, TO_TIMESTAMP_TZ(:15, 'YYYY-MM-DD HH24:MI:SS TZH:TZM'), :16, TO_TIMESTAMP_TZ(:17, 'YYYY-MM-DD HH24:MI:SS TZH:TZM'))
     """
     return sql_insert
 
@@ -83,9 +95,9 @@ def insert_to_transactiondma():
     # SQL command to insert data
     # FREEPDB1.JABAR.
     sql_insert = """
-    INSERT INTO TRANSACTIONDMA
-    (USER_ID, MCC_GROUP, MERCHANT_CATEGORY, RANGKING, PROMO_LOCATION)
-    VALUES (:1, :2, :3, :4, :5)
+    INSERT INTO TRANSACTION_DMA
+    (ID, USER_ID, MCC_GROUP, MERCHANT_CATEGORY, RANKING, UPDATE_BY, UPDATED_TIME, CREATED_BY, CREATED_TIME, PROMO_LOCATION)
+    VALUES (:1, :2, :3, :4, :5, :6, TO_TIMESTAMP_TZ(:7, 'YYYY-MM-DD HH24:MI:SS TZH:TZM'), :8, TO_TIMESTAMP_TZ(:9, 'YYYY-MM-DD HH24:MI:SS TZH:TZM'), :10)
     """
     return sql_insert
 
@@ -110,24 +122,33 @@ def main():
     
         table, df = get_arguments()
     
-        if len(df) <= 10000:
-            datas = get_data(df)
-        else:
-            datas = list(df.values)
-        
-        if table.upper()=='MASTERUSER':
+        # if len(df) <= 10000:
+        #     datas = get_data(df)
+        # else:
+        #     datas = list(df.values)
+        datas = list(df.values)
+
+        if table.upper()=='MASTER_USER':
             sql_insert= insert_to_masteruser()
-        elif table.upper()=='MASTERMERCHANT':
+        elif table.upper()=='MASTER_MERCHANT':
             sql_insert= insert_to_mastermerchant()
-        elif table.upper()=='MASTERPROMO':
-            sql_insert= insert_to_masterpromo()
-        elif table.upper()=='TRANSACTIONDMA':
+        elif table.upper()=='MASTER_PROMO_1':
+            sql_insert= insert_to_masterpromo_1()
+        elif table.upper()=='MASTER_PROMO_2':
+            sql_insert= insert_to_masterpromo_2()
+        elif table.upper()=='TRANSACTION_DMA':
             sql_insert= insert_to_transactiondma()
+        else :
+            print(f"There's no {table} table")
+            exit()
         
-    
+        
         print(f'Inserting {len(df)} data to {table} ... ')
         count = 1
         batch_size = 10000
+        batches = int(len(datas)/batch_size) if int(len(datas)/batch_size) > 0 else 1
+        print(f"Making {batches} batch(s) before inserting ... ")
+
         for i in range(0, len(datas), batch_size):
             # Create a cursor and execute the query
             cursor = connection.cursor()
